207c7039056e0d53bf864c87f1de2c82cad26ca1
Alec Larson <alec.stanford.larson@gmail.com>
2019-06-02 10:07:04 -0400

    1.0.2

---
+++ lib/recrawl.d.ts
@@ -0,0 +1,26 @@
+import { FileAdapter } from './fs';
+export declare type DirFilter = (dir: string, depth: number) => boolean;
+export declare type FileFilter = (file: string, name: string) => boolean;
+export declare type LinkFilter = (link: string, depth: number) => boolean;
+export declare type RecrawlOptions = Options;
+declare type Options = {
+    only?: string[];
+    skip?: string[];
+    deep?: boolean;
+    depth?: number;
+    enter?: DirFilter;
+    filter?: FileFilter;
+    follow?: boolean | number | LinkFilter;
+    adapter?: FileAdapter;
+};
+/** Create a crawl function, and crawl the given root immediately */
+export declare const crawl: (root: string, opts?: Options) => Promise<string[] | {
+    [name: string]: string | boolean;
+}>;
+/** Create a crawl function */
+export declare const recrawl: (opts?: Options) => (root: string, arg?: string[] | ((file: string, link: string | null) => void) | {
+    [name: string]: string | boolean;
+} | undefined) => Promise<string[] | {
+    [name: string]: string | boolean;
+}>;
+export {};
