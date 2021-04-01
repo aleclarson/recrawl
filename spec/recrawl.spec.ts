488ff577e943cb6b084f82abc5e3cb9bdbd42243
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-03-31 21:57:24 -0400

    test: import from src

--- spec/recrawl.spec.ts
+++ spec/recrawl.spec.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import { crawl } from '..'
+import { crawl } from '../src/recrawl'
 
 const root = path.join(__dirname, '__fixtures__', 'root')
 
